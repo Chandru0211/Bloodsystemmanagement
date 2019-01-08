5f24f5a654326231627128c6bdc8b1a0b99559b4
osgdev <osgdev@Capstone015.wipro.com>
2019-01-08 19:25:38 +0530

    Added Initial front-end contents

--- src/main/java/com/devops/bbm/controller/LoginServlet.java
+++ src/main/java/com/devops/bbm/controller/LoginServlet.java
@@ -36,6 +36,7 @@ public class LoginServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
+		response.sendRedirect("./search.html");
 	}
 
 }
